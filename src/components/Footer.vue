245fb4c6a191a2763182a0b925f242261cbb9a59
Marina-Hizhnyak <maryfadeeva1208@gmail.com>
2024-09-17 15:44:36 +0200

    projet_tailwind

---
+++ src/components/Footer.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <footer class="mt-auto py-3 bg-gray-100">
+    <!-- Code du footer -->
+  </footer>
+</template>
---
+++ src/components/Layout.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Nav from './Nav.vue';
+import Footer from './Footer.vue';
+import PrimaryButton from './PrimaryButton.vue';
+</script>
+
+<template>
+  <div class="flex flex-col min-h-screen">
+    <header>
+      <Nav>Accueil</Nav>
+    </header>
+
+    <main class="flex-grow py-5">
+      <div class="container mx-auto">
+        <slot name="title" />
+        <slot name="content" />
+      </div>
+      <PrimaryButton/>
+    </main>
+
+    <Footer />
+  </div>
+</template>
